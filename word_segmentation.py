207eb0da790354061660dcc8b8a7dcfa118de456
wada2005 <1871726+wada2005@users.noreply.github.com>
2019-11-17 00:47:40 +0900

    add

---
+++ word_segmentation.py
@@ -0,0 +1,17 @@
+import getopt
+import sys
+
+def usage():
+    print('usage: % word_segmentation.py indata outdata')
+    sys.exit(0)
+
+def main() :
+    args = sys.argv 
+    if len(args) == 3:
+        indata = args[1]
+        outdata = args[2]
+    else:
+        usage ()
+
+if __name__ == "__main__":
+    main ()
