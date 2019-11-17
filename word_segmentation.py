0f40040da99c04df10be7366dd651275a69cc82b
wada2005 <1871726+wada2005@users.noreply.github.com>
2019-11-17 11:29:42 +0900

    add

--- word_segmentation.py
+++ word_segmentation.py
@@ -1,14 +1,28 @@
-import getopt
 import sys
+import getopt
+import defaultdict
 
 def usage():
     print('usage: % word_segmentation.py indata outdata')
     sys.exit(0)
 
-def ngram(text, n):
-    
+def ngram(words, n):
+    nn = list(zip(*(words[i:] for i in range(n))))
+    dic = defaultdict(int)
+    for line in nn:
+        dic[line] += 1
+    line = ""
+    for k, v in dic.items():
+        line = line + k + ":" + v + " "
+    line = line + "\n"
+    return line
 
-
+def process_ngram(infile, outfile, n):
+    with open(infile, "r") as fi:
+        with open(outfile, "w") as fo:
+            for line in fi:
+                wordcount = ngram(line, n)
+                fo. write(wordcount)
 
 def main() :
     args = sys.argv 
